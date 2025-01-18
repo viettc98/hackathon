00dd783ab2f34b28a1ca67a0efd33d52f925cd04
Minh Lê <150336785+minhlhc98@users.noreply.github.com>
2025-01-18 22:15:29 +0700

    fix: Wrong network

--- read-to-earn-plugin/src/providers/WalletAdapterProvider.tsx
+++ read-to-earn-plugin/src/providers/WalletAdapterProvider.tsx
@@ -2,12 +2,12 @@ import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 import { WagmiProvider } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { viction, victionTestnet } from "wagmi/chains";
 
 const config = getDefaultConfig({
   appName: "Read To Earn",
   projectId: "READ_TO_EARN",
-  chains: [mainnet],
+  chains: [viction, victionTestnet],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
