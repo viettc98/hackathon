2d4b859a90a3043068b46e4b595a15acc690f46c
Minh Lê <150336785+minhlhc98@users.noreply.github.com>
2025-01-18 19:47:49 +0700

    fix: Crash app

--- read-to-earn-plugin/src/components/VoiceRecorder/index.tsx
+++ read-to-earn-plugin/src/components/VoiceRecorder/index.tsx
@@ -1,11 +1,21 @@
 import { AudioOutlined } from "@ant-design/icons";
+import { useVoiceContext } from "../../hooks/useVoiceContext";
 
 const VoiceRecorder = () => {
+  const { isRecording, setIsRecording } = useVoiceContext();
+  const toggleRecordingState = () => {
+    setIsRecording(!isRecording);
+  };
   return (
     <div
+      onClick={toggleRecordingState}
       className="flex size-24 mt-auto items-center justify-center cursor-pointer rounded-xl bg-purple-600"
     >
-      <AudioOutlined className="text-4xl text-white" />
+      {isRecording ? (
+        <img src={"/public/audio.svg"} className="size-8" />
+      ) : (
+        <AudioOutlined className="text-4xl text-white" />
+      )}
     </div>
   );
 };
