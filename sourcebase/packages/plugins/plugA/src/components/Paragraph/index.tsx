fd80f8e2b19cfd315ac3d05a863b3bf8c5985fdd
duy-nd51 <duyndhk@coin98.finance>
2025-01-19 09:22:39 +0700

    chore: paragraph issue

--- sourcebase/packages/plugins/plugA/src/components/Paragraph/index.tsx
+++ sourcebase/packages/plugins/plugA/src/components/Paragraph/index.tsx
@@ -7,7 +7,7 @@ const Paragraph = () => {
   const { result, script, finalTranscript } = useVoice();
   return (
     <div className="rounded-lg text-center mx-2 max-w-full p-4 text-wrap overflow-auto text-xl bg-white/15">
-      {/* {result ? (
+      {result ? (
         <>
           <p>Result: {result}%</p>
           {highlightMatchedWords(
@@ -17,8 +17,7 @@ const Paragraph = () => {
         </>
       ) : (
         script
-      )} */}
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique laboriosam neque unde libero. Doloribus amet id libero perspiciatis quod, nesciunt reiciendis nulla commodi modi asperiores inventore, consequuntur ab voluptatibus temporibus dolore veritatis, harum debitis esse. Eveniet iste eaque, molestias modi nemo porro omnis nulla mollitia saepe facere? Aliquam tenetur commodi quas reiciendis ea deleniti praesentium voluptates laborum ab, accusantium beatae possimus corporis officia odit maiores dicta. Totam soluta deleniti sint voluptatem, rem dolor, tenetur odio ipsa, quidem pariatur accusantium possimus facere! Labore omnis iste sequi nisi nesciunt non error facilis consequatur quam minus. Eos magnam accusamus magni illo sed praesentium!
+      )}
     </div>
   );
 };
